59583cb3099291192ab76baeb6df67699eeb8c26
DarkMoon53 <andrewjvsistemas@gmail.com>
2022-10-15 11:07:12 -0500

    agregando modelos, controladores

---
+++ database/migrations/2022_10_15_155805_create_cliente_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cliente', function (Blueprint $table) {
+            $table->id();
+            $table->string("nombre",50)->nullable(false);
+            $table->string("apellido",51)->nullable(false);
+            $table->string("direccion",100)->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cliente');
+    }
+};
